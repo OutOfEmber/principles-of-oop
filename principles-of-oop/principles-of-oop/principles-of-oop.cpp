f91f77cb7881cbaaa46d1cd045be398acc348330
Терещенко Вадим Александрович <v.tereshenko@mkit.local>
2026-01-30 11:53:12 +0300

    ya ebal cpp

--- principles-of-oop/principles-of-oop/principles-of-oop.cpp
+++ principles-of-oop/principles-of-oop/principles-of-oop.cpp
@@ -5,24 +5,35 @@
 #include <ctime>
 #include <chrono>
 using namespace std;
-class  Summator {
 
-};
-void logToFile(const string& filename) {
-    ofstream log1("wasd.log", ios_base::app);
-    if (log1.is_open()) {
+void logToFile(const string& message) {
+    ofstream log("logi.log", ios_base::app);
+    if (log.is_open()) {
         auto now = chrono::system_clock::now();
         time_t now_time = chrono::system_clock::to_time_t(now);
-        log1 << ctime(&now_time) << ": " << filename << endl;
+        log << ctime(&now_time) << "->" << message << endl;
         cout << "Записано в файл" << endl;
-        log1.close();
+        log.close();
     }
     else {
         cerr << "Не удалось открыть" << endl;
     }
 }
+void logToScreen() {
+    ifstream log("logi.log");
+    if (!log.is_open()) {
+        cerr << "Не удалось открыть" << endl;
+        return;
+    }
+    string line;
+    while (getline(log , line)) {
+        cout << line << endl;
+    }
+    log.close();
+}
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    logToFile("udalenie");
+    logToFile("h");
+    logToScreen();
 }
